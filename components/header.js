ba71beec0affb15b4b9d3253e65400694f7f97c4
Eldar <eldarg76@gmail.com>
2020-09-08 16:03:35 +0500

    hot-fix

--- components/header.js
+++ components/header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "./Link";
+import Link from "./link";
 import Button from "./button";
 import { withRouter } from "next/router";
 //import { withRouter } from "react-router-dom";
